e33f06425a9e7d4c1c101211ef5d537e5bbdbec2
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-01-16 15:25:00 +0100

    create home page

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,10 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+import { db } from '$lib/server/database';
+
+
+export const load: PageServerLoad = async({ locals }) => {
+    if(!locals.currentAdmin) {
+        throw redirect(302, '/login');
+    }
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+</script>
+
+<section class="w-full h-full flex flex-row">
+	<p>home</p>
+</section>
