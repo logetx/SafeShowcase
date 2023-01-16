45a0453063de758c84fb4eed33900374f69f5a5f
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-01-16 15:24:45 +0100

    create login route

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,39 @@
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+import { db } from '$lib/server/database';
+
+
+export const load: PageServerLoad = async({ locals }) => {
+    if(locals.currentAdmin) {
+        throw redirect(302, '/');
+    }
+}
+
+
+export const actions: Actions = {
+	async default({ request, cookies }) {
+        const data = await request.formData();
+
+        const user = await db.admin.findUnique({
+            where: { username: data.get('username')?.toString() }
+        });
+
+        if(!user) {
+            return fail(400, {success: false, message: 'Invalid user'});
+        }
+
+        if (user.password !== data.get('password')?.toString() ?? '') {
+            return fail(400, {success: false, message: 'Invalid password'});
+        }
+
+		cookies.set('session', user.id.toString(), {
+			path: '/',
+            secure: true,
+            httpOnly: true,
+            sameSite: 'strict',
+            maxAge: 60 * 60 * 24 * 7
+		});
+
+		throw redirect(302, '/');
+	}
+}
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+</script>
+
+<section class="w-full h-full flex flex-row">
+	<div class="w-4/6 image"></div>
+	<div class="w-2/6 flex flex-col items-center justify-center">
+		<form method="post">
+			<h1 class="text-4xl font-bold mb-2">Login</h1>
+			<div class="flex flex-col">
+				<label for="username">Username</label>
+				<input class="bg-zinc-700" autocomplete="off" type="text" name="username" id="username" />
+			</div>
+			<div class="flex flex-col">
+				<label for="password">Password</label>
+				<input class="bg-zinc-700" autocomplete="off" type="password" name="password" id="password" />
+			</div>
+			<button class="mt-2 p-1 text-zinc-300 bg-zinc-700" type="submit">Submit</button>
+		</form>
+	</div>
+</section>
+
+<style>
+	.image {
+		background-image: url("/safe_unsplash.jpg");
+		background-size: cover;
+		background-position: center;
+	}
+</style>
