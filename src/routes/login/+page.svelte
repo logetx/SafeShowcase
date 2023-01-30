16fd60aa98c82bc8aa7b96a80c9c0f4973dfdd64
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-01-30 17:13:13 +0100

    create footer in login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
-
+	import { enhance } from "$app/forms";
 </script>
 
 <section class="w-full h-full flex flex-row">
 	<div class="w-4/6 image"></div>
-	<div class="w-2/6 flex flex-col items-center justify-center">
-		<form method="post">
+	<div class="w-2/6 flex flex-col items-center justify-between">
+		<span></span>
+		<form 
+			method="post"
+			use:enhance
+		>
 			<h1 class="text-4xl font-bold mb-2">Login</h1>
 			<div class="flex flex-col">
 				<label for="username">Username</label>
@@ -17,6 +21,9 @@
 			</div>
 			<button class="mt-2 p-1 text-zinc-300 bg-zinc-700" type="submit">Submit</button>
 		</form>
+		<footer class="mb-4">
+			Nulltech &copy; 2023
+		</footer>
 	</div>
 </section>
 
