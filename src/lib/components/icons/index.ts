452d50d2d56a49e6a6efbfeab1fcfc849705db90
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-01-30 22:15:19 +0100

    create icons and management for them

---
+++ src/lib/components/icons/index.ts
@@ -0,0 +1,19 @@
+export {
+    default as user
+} from '$lib/components/icons/user.svelte';
+
+export {
+    default as users
+} from '$lib/components/icons/users.svelte';
+
+export {
+    default as logout
+} from '$lib/components/icons/logout.svelte';
+
+export {
+    default as home
+} from '$lib/components/icons/home.svelte';
+
+export {
+    default as clipboardDocument
+} from '$lib/components/icons/clipboardDocument.svelte';
