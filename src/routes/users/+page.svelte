f7140c887f9897cb77261d2f1d9aefa8cafbffcb
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-01-30 22:17:07 +0100

    update structure for users page

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -1,7 +1,29 @@
 <script lang="ts">
+	import { enhance } from "$app/forms";
+
+
 
 </script>
 
-<section>
-    
+<section class="w-full h-full flex flex-row justify-center gap-16">
+    <div class="w-5/12 bg-zinc-700 my-8">
+
+    </div>
+    <div class="flex flex-col justify-between w-5/12 my-8 gap-16">
+        <div class="w-full h-full bg-zinc-700">
+            <form 
+                method="post"
+                use:enhance
+            >
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" />
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+        <div class="w-full h-full bg-zinc-700">
+
+        </div>
+    </div>
 </section>
