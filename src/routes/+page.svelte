9ca630a97a30492ffc465b34d6d0dd176e3b86df
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-02-02 11:29:53 +0100

    upload banner for dashboard

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 </script>
 
 <section class="w-full h-full flex flex-row">
-	<h1 class="text-lg text-zinc-700">Welcome</h1>
+	<div class="w-full h-20 flex flex-row items-center m-6 gap-4">
+		<h1 class="w-4/12 text-2xl text-zinc-700">Welcome, {$page.data.currentAdmin.username}</h1>
+		<div class="w-full h-full image"></div>
+	</div>
+
 </section>
+
+<style>
+	.image {
+		background-image: url("/armored_vehicles.png");
+		background-size: cover;
+		background-position: center;
+	}
+</style>
