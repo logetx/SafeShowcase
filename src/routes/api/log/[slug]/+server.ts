0ecbb3e5b14b26c2e5bb4320009bd059c479d3cd
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-02-03 23:42:10 +0100

    set query as default instead of testing generator

--- src/routes/api/log/[slug]/+server.ts
+++ src/routes/api/log/[slug]/+server.ts
@@ -8,19 +8,19 @@ export const POST: RequestHandler = async ({ locals, params }) => {
         return json({ error: 'Not logged in' }, { status: 401 });
     }
 
-    // const data = await db.logs.findMany({
-    //     where: {
-    //         card_hex: params.slug,
-    //     },
-    //     orderBy: {
-    //         createdAt: 'desc',
-    //     }
-    // });
+    const data = await db.logs.findMany({
+        where: {
+            card_hex: params.slug,
+        },
+        orderBy: {
+            createdAt: 'desc',
+        }
+    });
 
-    const data = Array.from({ length: 100 }, (_, i) => ({
-        createdAt: new Date().toISOString(),
-        status: true,
-    }));
+    // const data = Array.from({ length: 100 }, (_, i) => ({
+    //     createdAt: new Date().toISOString(),
+    //     status: true,
+    // }));
 
     return json(data);
 }
