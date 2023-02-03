ce27b78a4ccc32d897f736a992a5759ccaaa4e74
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-02-03 20:50:52 +0100

    fix style and schema for all the page

--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -28,18 +28,12 @@
 
         currentSelected = hex;
         currentUser = users.find((user) => user.hex === hex) as unknown as UserClass;
+        userLogs = await getUserLogs(hex);
     }
 
     async function getUserLogs(hex: string) {
-        const response = await fetch('/api/log/', {
-            method: 'POST',
-            body: JSON.stringify({ hex: hex }),
-            headers: {
-                'content-type': 'application/json'
-            }
-        });
-
-        userLogs = await response.json();
+        const response = await fetch(`/api/log/${hex}`, {method: 'POST'});
+        return await response.json();
     }
 
     // $: users = Array.from({ length: 100 }, (_, i) => ({
@@ -50,28 +44,30 @@
 </script>
 
 <section class="w-full h-full flex items-center justify-center">
-    <div class="w-5/6 h-full flex flex-col gap-8 py-8">
-        <div class="w-full flex flex-row gap-8">
-            <div class="w-full flex flex-grow flex-shrink flex-col bg-zinc-700 p-2">
-                <h2 class="text-lg text-zinc-300">Users</h2>
-                <div class="w-full overflow-y-scroll border-t-[1px] border-zinc-300 select-none">
-                    <div class="flex flex-row text-sm text-zinc-300">
+    <div class="w-5/6 h-full flex flex-col gap-8 py-8"> <!-- Center Box -->
+        <div class="w-full flex flex-row gap-8"> <!-- Top Box -->
+            <div class="w-full h-[16.0625rem] bg-zinc-700 p-2"> <!-- User List Box -->
+                <h2 class="text-lg text-zinc-300 h-[11%]">Users</h2>
+                <div class="w-full border-t-[1px] border-zinc-300 select-none max-h-[89%] overflow-y-auto">
+                    <div class="sticky top-0 flex flex-row text-sm font-bold text-zinc-300 backdrop-blur">
                         <span class="w-1/3">Hex</span>
                         <span class="w-1/3">Name</span>
                         <span class="w-1/3">Security Number</span>
                     </div>
-                    {#each users as user}
-                        <!-- svelte-ignore a11y-click-events-have-key-events -->
-                        <div class={rowBaseClass} id={user.hex} on:click={() => selectUser(user.hex)}>
-                            <span class="w-1/3">{user.hex}</span>
-                            <span class="w-1/3">{user.name}</span>
-                            <span class="w-1/3">{user.security_number}</span>
-                        </div>
-                    {/each}
+                    <div class="w-full">
+                        {#each users as user}
+                            <!-- svelte-ignore a11y-click-events-have-key-events -->
+                            <div class={rowBaseClass} id={user.hex} on:click={() => selectUser(user.hex)}>
+                                <span class="w-1/3">{user.hex}</span>
+                                <span class="w-1/3">{user.name}</span>
+                                <span class="w-1/3">{user.security_number}</span>
+                            </div>
+                        {/each}
+                    </div>
                 </div>
             </div>
-            <div class="w-full h-fit bg-zinc-700 text-zinc-300 text-sm p-2">
-                <h1 class="text-xl">Create new user</h1>
+            <div class="w-full h-fit bg-zinc-700 text-zinc-300 text-sm p-2"> <!-- Create User Box -->
+                <h2 class="text-xl">Create new user</h2>
                 <form
                     class="border-t-[1px] border-zinc-300"
                     action="?/create"
@@ -110,44 +106,56 @@
                 </form>
             </div>
         </div>
-        <div class="w-full h-fit bg-zinc-700 p-2">
+        <div class="w-full h-fit bg-zinc-700 p-2"> <!-- Bottom Box  -  User Info Box -->
             <h1 class="text-xl text-zinc-300">User details</h1>
-            <div class="flex flex-row border-t-[1px] border-zinc-300">
-                <div class="w-full flex flex-col text-zinc-300 gap-1">
+            <div class="flex flex-row border-t-[1px] border-zinc-300"> <!-- User Info Box -->
+                <div class="w-full flex flex-col justify-between text-zinc-300 mt-1">
                     <div class="flex flex-col">
+                        <!-- svelte-ignore a11y-label-has-associated-control -->
                         <label class="text-xs font-bold">HEX</label>
                         <span class="h-5 px-1 text-sm bg-zinc-300 text-zinc-700">{currentUser?.hex || ""}</span>
                     </div>
                     <div class="flex flex-col">
+                        <!-- svelte-ignore a11y-label-has-associated-control -->
                         <label class="text-xs font-bold">NAME</label>
                         <span class="h-5 px-1 text-sm bg-zinc-300 text-zinc-700">{currentUser?.name || ""}</span>
                     </div>
                     <div class="flex flex-col">
+                        <!-- svelte-ignore a11y-label-has-associated-control -->
                         <label class="text-xs font-bold">SURNAME</label>
                         <span class="h-5 px-1 text-sm bg-zinc-300 text-zinc-700">{currentUser?.surname || ""}</span>
                     </div>
                     <div class="flex flex-col">
+                        <!-- svelte-ignore a11y-label-has-associated-control -->
                         <label class="text-xs font-bold">EMAIL</label>
                         <span class="h-5 px-1 text-sm bg-zinc-300 text-zinc-700">{currentUser?.email || ""}</span>
                     </div>
                     <div class="flex flex-col">
+                        <!-- svelte-ignore a11y-label-has-associated-control -->
                         <label class="text-xs font-bold">PHONE NUMBER</label>
                         <span class="h-5 px-1 text-sm bg-zinc-300 text-zinc-700">{currentUser?.phone_number || ""}</span>
                     </div>
                     <div class="flex flex-col">
+                        <!-- svelte-ignore a11y-label-has-associated-control -->
                         <label class="text-xs font-bold">SECURITY NUMBER</label>
                         <span class="h-5 px-1 text-sm bg-zinc-300 text-zinc-700">{currentUser?.security_number || ""}</span>
                     </div>
                 </div>
-                <div class="w-full h-full flex flex-col mx-2 mt-2">
-                    <!-- {#each getUserLogs(currentUser.hex) as log}
-                        <div class="flex flex-row text-sm text-zinc-300">
-                            <span class="w-1/3">{log.createdAt}</span>
-                            <span class="w-1/3">{log.status}</span>
-                        </div>
-                    {/each} -->
+                <div class="w-full h-full flex flex-col mx-4 mt-1"> <!-- User Logs Box -->
+                    <div class="flex flex-row text-xs font-bold text-zinc-300">
+                        <span class="w-1/2">Last Log</span>
+                        <span class="w-1/2">Status</span>
+                    </div>
+                    <div class="max-h-[12.5rem] overflow-y-scroll">
+                        {#each userLogs as log}
+                            <div class="flex flex-row text-sm text-zinc-300">
+                                <span class="w-1/2">{log.createdAt}</span>
+                                <span class="w-1/2">{log.status}</span>
+                            </div>
+                        {/each}
+                    </div>
                 </div>
-                <div class="w-2/12 flex flex-col">
+                <div class="w-2/12 flex flex-col"> <!-- User Buttons -->
                     <form
                         action="?/delete" 
                         method="post"
@@ -155,7 +163,7 @@
                     >
                         <input type="hidden" name="hex" value={currentUser?.hex} />
                         {#if currentUser?.hex}
-                            <button class="w-full text-sm p-1 mt-2 bg-zinc-300 text-zinc-700" type="submit">Delete</button>
+                            <button class="w-full text-sm p-1 mt-1 bg-zinc-300 text-zinc-700" type="submit">Delete</button>
                         {/if}
                     </form>
                 </div>
