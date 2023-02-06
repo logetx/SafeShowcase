1cc475a5f6b4d770cdaf53bb61a29a05f937acf9
Alessandro <59316554+logeFox@users.noreply.github.com>
2023-02-06 21:20:26 +0100

    fix wrong name for api

--- src/routes/api/auth/+server.ts
+++ src/routes/api/auth/+server.ts
@@ -4,7 +4,11 @@ import type { RequestHandler } from '@sveltejs/kit';
 
 
 export const POST: RequestHandler = async ({ request }) => {
+    console.log(request)
+    
     const { hex, secret_key } = await request.json();
+
+    console.log(hex, secret_key)
 
     const admin_access = await db.admin.findFirst({
         where: {
@@ -28,13 +32,28 @@ export const POST: RequestHandler = async ({ request }) => {
         status = false;
     }
 
-    await db.logs.create({
+    const log = await db.logs.create({
         data: {
             card_hex: hex,
             action: status ? "Access granted" : "Access denied",
-            status: status
+            status: status,
         }
     });
+
+    if (user) {
+        await db.logs.update({
+            where: {
+                id: log.id
+            },
+            data: {
+                users: {
+                    connect: {
+                        hex: user.hex
+                    }
+                }
+            }
+        });
+    }
 
     return json(status);
 }
